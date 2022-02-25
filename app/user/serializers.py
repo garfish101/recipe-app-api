0a183b3997faf70973cf93c4f97cf88b3dbfb505
Gareth <gareth01montile@gmail.com>
2022-02-24 19:41:33 -0600

    whatever

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,6 +18,15 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user,setting the password correctly and retun it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user authentication objects"""
